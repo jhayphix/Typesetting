f1e51b7277e25fd56d596981028919faa4c02b84
jhayphix <jhayphix05@gmail.com>
2024-03-20 01:00:33 +0000

    Updated_final_project_rcode

--- Final_year_project/Rcode/Chapter_4-Goil.R
+++ Final_year_project/Rcode/Chapter_4-Goil.R
@@ -8,6 +8,10 @@ library(quantmod) # Package for volatility
 # --------------------------------------------------
 # Root
 # --------------------------------------------------
+year_start = c(2018, 1)
+year_end = c(2024, 3)
+freq = 12
+
 # Define a list containing color groups
 colors <- list(
   blue = c("blue", "skyblue", "dodgerblue", "navy"),
@@ -26,6 +30,8 @@ diesel_col <- colors_def$blue
 super_col <- colors_def$red
 lwd_def <- 3
 
+
+
   
 # --------------------------------------------------
 # Data collection
@@ -42,25 +48,27 @@ attach(goil_data)
 missing_values <- sum(is.na(goil_data$Diesel)) + sum(is.na(goil_data$Super))
 
 # ... Convert data to time series object
-diesel_ts <- ts(Diesel, start=c(2018, 1), frequency=12)
-super_ts <- ts(Super, start=c(2018, 1), frequency=12)
+diesel_ts <- ts(Diesel, start=year_start, frequency=feq)
+super_ts <- ts(Super, start=year_start, frequency=feq)
 
-# ... Visualize data
-plot(diesel_ts, main="Time Series Plot of Diesel Prices", ylab="Price", xlab="Year", lwd=lwd_def, col=diesel_col)
-plot(super_ts, main="Time Series Plot of Super Prices", ylab="Price", xlab="Year", lwd=lwd_def, col=super_col)
-
-# --------------------------------------------------
-# Splitting Data into Training and Testing Sets
-# --------------------------------------------------
+# ... Splitting Data into Training and Testing Sets
 train_size <- 0.8  # 80% training data, 20% testing data
 num_obs <- length(diesel_ts)
 train_index <- 1:round(train_size * num_obs)
 test_index <- (round(train_size * num_obs) + 1):num_obs
 
-diesel_train <- diesel_ts[train_index]
-diesel_test <- diesel_ts[test_index]
-super_train <- super_ts[train_index]
-super_test <- super_ts[test_index]
+diesel_train <- ts(diesel_ts[train_index], start=year_start, frequency=freq)
+diesel_test <- ts(diesel_ts[test_index], end=year_end, frequency=freq)
+
+super_train <- ts(super_ts[train_index], start=year_start, frequency=freq)
+super_test <- ts(super_ts[test_index], start=year_start, frequency=freq)
+
+# ... Visualize data
+plot(diesel_ts, main="Time Series Plot of Diesel Prices", ylab="Price", xlab="Year", lwd=lwd_def, col=colors$green[2], ylim = range(c(diesel_train, diesel_test)))
+lines(diesel_train, lwd=lwd_def, col=diesel_col)
+
+plot(super_ts, main="Time Series Plot of Super Prices", ylab="Price", xlab="Year", lwd=lwd_def, col=colors$green[2], ylim = range(c(super_train, super_test)))
+lines(super_train, lwd=lwd_def, col=super_col)
 
 # --------------------------------------------------
 # Exploratory Data Analysis (EDA)
@@ -70,10 +78,28 @@ summary(diesel_train)
 summary(super_train)
 
 # ... Stationarity
-adf.test(diesel_train)
+stationaryTest <- function(data, title=""){
+  line = "\n ---------- ***** ----------- \n"
+  cat(line); cat(title) ; cat(line)
+  adf = adf.test(data); print(adf); cat(line)
+  pp = pp.test(data); print(pp); cat(line)
+  kpss = kpss.test(data); print(kpss); cat(line)
+}
+stationaryTest(diesel_train, "Diesel Training")
+stationaryTest(super_train, "Super Training")
 
 # ... Differencing
+ndiffs(diesel_train)
+ndiffs(super_train)
+diesel_train_diff <- diff(diesel_train)
+super_train_diff <- diff(super_train)
 
+# ... Visualize differenced data
+plot(diesel_train_diff, main="Time Series Plot of Diesel Prices", ylab="Price", xlab="Year", lwd=lwd_def, col=diesel_col)
+plot(super_train_diff, main="Time Series Plot of Super Prices", ylab="Price", xlab="Year", lwd=lwd_def, col=super_col)
+
+stationaryTest(diesel_train_diff)
+stationaryTest(super_train_diff)
 
 # --------------------------------------------------
 # Calculate Volatility
@@ -118,6 +144,7 @@ adf.test(diesel_train)
 # --------------------------------------------------
 # 
 # --------------------------------------------------
+
 
 
 
